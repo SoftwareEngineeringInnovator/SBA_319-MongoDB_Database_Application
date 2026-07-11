aa962fd6a0b62c83694316247ad4d373e35dcb25
SoftwareEngineeringInnovator <fredychilito@outlook.com>
2026-07-11 18:14:48 -0400

    add comment filed for the Comments.js file

---
+++ models/Comments.js
@@ -0,0 +1,22 @@
+// Comment model
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema(
+    {
+        message: {
+            type: String,
+            required: [true, "Comment message is required"],
+            trim: true,
+            minlength: [5, "Comment message must be at least 5 characters long"],
+        },
+    },
+
+    {
+        // Time creation for incident
+        timestamps: true,
+    }
+);
+
+const Comment = mongoose.model("Comment", commentSchema);
+
+export default Comment;
