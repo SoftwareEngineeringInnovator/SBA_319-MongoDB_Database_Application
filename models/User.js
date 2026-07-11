e803100b8f22e9a25d1197a764e583b9f55dde9a
SoftwareEngineeringInnovator <fredychilito@outlook.com>
2026-07-11 17:28:54 -0400

    add role and department fields to the schema/ user model, successfully tested the GET route for the new fields and prior data

--- models/User.js
+++ models/User.js
@@ -8,13 +8,26 @@ const userSchema = new mongoose.Schema(
             required: [true, "User name is required"],
             trim: true,
         },
-        
+
         email: {
             type: String,
             required: [true, "User email is required"],
             trim: true,
             lowercase: true,
             unique: true,
+        },
+
+        role: {
+            type: String,
+            required: [true, "User role is required"],
+            enum: ["Reporter", "SOC Analyst", "Manager", "Admin"],
+            default: "Reporter",
+        },
+
+        department: {
+            type: String,
+            required: [true, "Department is required"],
+            trim: true,
         },
 
     },
