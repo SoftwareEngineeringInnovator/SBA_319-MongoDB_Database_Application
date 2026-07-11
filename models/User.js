cceed29acd7b6eeba971af586db257dfe271c7a5
SoftwareEngineeringInnovator <fredychilito@outlook.com>
2026-07-11 16:40:47 -0400

    add user name, timestamps to User.js file

---
+++ models/User.js
@@ -0,0 +1,22 @@
+// User model will use this interface to report incidents
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "User name is required"],
+      trim: true,
+    },
+
+  },
+
+  {
+    // Time creation for incident
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
