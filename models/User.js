8b1ef9580418d07ba0ece3f1d9d51ea65ccb882d
SoftwareEngineeringInnovator <fredychilito@outlook.com>
2026-07-11 17:10:36 -0400

    add email field to the schema/ user model

--- models/User.js
+++ models/User.js
@@ -2,20 +2,27 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: [true, "User name is required"],
-      trim: true,
+    {
+        name: {
+            type: String,
+            required: [true, "User name is required"],
+            trim: true,
+        },
+        
+        email: {
+            type: String,
+            required: [true, "User email is required"],
+            trim: true,
+            lowercase: true,
+            unique: true,
+        },
+
     },
 
-  },
-  
-
-  {
-    // Time creation for incident
-    timestamps: true,
-  }
+    {
+        // Time creation for incident
+        timestamps: true,
+    }
 );
 
 const User = mongoose.model("User", userSchema);
